6506a9b451cea2ace5297f184b69a613b00eafb1
Tiffany Zhu <tiffanyzhu19@gmail.com>
2021-09-21 18:18:58 -0400

    update config for deployment

--- random_circuit_app/__init__.py
+++ random_circuit_app/__init__.py
@@ -6,3 +6,4 @@ app = Flask(__name__)
 # For the forms
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SESSION_COOKIE_SECURE'] = False  # to fix issue when deploying?
