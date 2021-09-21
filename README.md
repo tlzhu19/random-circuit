e647d9b7ab4840da263d6cc9066a923806a47eaa
Tiffany Zhu <tiffanyzhu19@gmail.com>
2021-09-21 10:30:57 -0400

    restructure data and add requirements

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # random-circuit
+
+Setup:
+
+1. clone git repo
+```
+git clone https://github.com/tlzhu19/random-circuit.git
+```
+
+2. virtual environment
+```
+cd random-circuit
+
+virtualenv venv 
+
+source venv/bin/activate
+```
+
+3. install requirements
+```
+pip install -r requirements.txt
+```
+
+4. run app
+```
+python app.py
+```
+
+5. when done, deactivate venv
+```
+deactivate
+```
+
--- random_circuit_app/app.py
+++ random_circuit_app/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+# current_app stores a reference to the global application object.
+from flask import Flask, render_template, current_app
+import pandas as pd
+import os
 
 app = Flask(__name__)
+app.exercises_df = pd.read_csv(os.path.join('data', 'exercises_expanded.csv'))  # load data once, bind to app
 
 
 @app.route('/')
 def index():
+    print(current_app.exercises_df.head())
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
